e9f52b46fc8c1eb6fd8241434f16a1ab1d76aac9
ccrause <christo.crause@gmail.com>
2023-03-17 07:54:17 +0200

    The TIOCGSERIAL ioctl is not supported by cp210x and ch430 drivers on Linux (5.4.0), replace with tcgetattr call. Also general cleanup of code.

--- gui/serialobject.pas
+++ gui/serialobject.pas
@@ -3,30 +3,15 @@ unit serialobject;
 interface
 
 uses
-  serial, sysutils;
-
-const
-  NCCS = 19;
+  serial, sysutils {$ifndef WINDOWS}, termio{$endif};
 
 type
-  {$PACKRECORDS C}
-    Termios2 = record
-      c_iflag,
-      c_oflag,
-      c_cflag,
-      c_lflag  : cardinal;
-      c_line   : char;
-      c_cc     : array[0..NCCS-1] of byte;
-      c_ispeed,
-      c_ospeed : cardinal;
-    end;
-
   { TSerialObj }
 
   TSerialObj = class
   private
     FBaudRate: integer;
-    {$IFNDEF WINDOWS}Ftios: Termios2;{$ENDIF}
+    {$IFNDEF WINDOWS}Ftios: Termios;{$ENDIF}
     FHandle: THandle;
     procedure FSetParams(ByteSize: Integer; Parity: TParityType; StopBits: Integer;
       Flags: TSerialFlags);
@@ -56,41 +41,17 @@ type
 function GetSerialPortNames: string;
 
 const
-  TCGETS2 = $802C542A; //longint(2150388778);
-  TCSETS2 = $402C542B; //1076646955;
-  ENOTTY  = 25;
+  TCGETS2 = $802C542A;
+  TCSETS2 = $402C542B;
 
 implementation
 
 uses
   {$IFDEF UNIX}
-  termio, BaseUnix, errors;
+  BaseUnix, errors;
   {$ELSE}
   windows, registry, classes;
   {$ENDIF}
-
-{ Translated from include/linux/serial.h }
-type
-  TSerialStruct = packed record
-    typ: Integer;
-    line: Integer;
-    port: Cardinal;
-    irq: Integer;
-    flags: Integer;
-    xmit_fifo_size: Integer;
-    custom_divisor: Integer;
-    baud_base: Integer;
-    close_delay: Word;
-    io_type: Char;
-    reserved_char: Char;
-    hub6: Integer;
-    closing_wait: Word; // time to wait before closing
-    closing_wait2: Word; // no longer used...
-    iomem_base: ^Char;
-    iomem_reg_shift: Word;
-    port_high: Cardinal;
-    iomap_base: LongWord; // cookie passed into ioremap
-  end;
 
 { TSerialObj }
 
@@ -267,97 +228,95 @@ begin
   inherited Destroy;
 end;
 
-////////////////////////////////////////////////////////////////////////
-  {$IFDEF MSWINDOWS}
-  function GetSerialPortNames: string;
-  var
-    reg: TRegistry;
-    l, v: TStringList;
-    n: integer;
-  begin
-    l := TStringList.Create;
-    v := TStringList.Create;
-    reg := TRegistry.Create;
-    try
-  {$IFNDEF VER100}
-  {$IFNDEF VER120}
-      reg.Access := KEY_READ;
-  {$ENDIF}
-  {$ENDIF}
-      reg.RootKey := HKEY_LOCAL_MACHINE;
-      reg.OpenKey('\HARDWARE\DEVICEMAP\SERIALCOMM', false);
-      reg.GetValueNames(l);
-      for n := 0 to l.Count - 1 do
-        v.Add(reg.ReadString(l[n]));
-      Result := v.CommaText;
-    finally
-      reg.Free;
-      l.Free;
-      v.Free;
-    end;
+{$IFDEF MSWINDOWS}
+function GetSerialPortNames: string;
+var
+  reg: TRegistry;
+  l, v: TStringList;
+  n: integer;
+begin
+  l := TStringList.Create;
+  v := TStringList.Create;
+  reg := TRegistry.Create;
+  try
+{$IFNDEF VER100}
+{$IFNDEF VER120}
+    reg.Access := KEY_READ;
+{$ENDIF}
+{$ENDIF}
+    reg.RootKey := HKEY_LOCAL_MACHINE;
+    reg.OpenKey('\HARDWARE\DEVICEMAP\SERIALCOMM', false);
+    reg.GetValueNames(l);
+    for n := 0 to l.Count - 1 do
+      v.Add(reg.ReadString(l[n]));
+    Result := v.CommaText;
+  finally
+    reg.Free;
+    l.Free;
+    v.Free;
   end;
-  {$ENDIF}
-  {$IFNDEF MSWINDOWS}
-  // Idea adapted from PascalSCADA project's serialport.pas file:
-  // http://pascalscada.svn.sourceforge.net/viewvc/pascalscada/trunk/serialport.pas?revision=702&view=markup
-  // More prefixes exist see e.g. :
-  // http://comments.gmane.org/gmane.comp.ide.lazarus.general/46750
+end;
+{$ENDIF}
+{$IFNDEF MSWINDOWS}
+// Idea adapted from PascalSCADA project's serialport.pas file:
+// http://pascalscada.svn.sourceforge.net/viewvc/pascalscada/trunk/serialport.pas?revision=702&view=markup
+// More prefixes exist see e.g. :
+// http://comments.gmane.org/gmane.comp.ide.lazarus.general/46750
 
-  {$IFDEF UNIX}
-  var
-    {$IFDEF LINUX}
-    PortPrefix: array[0..1] of string = ({'ttyS',} 'ttyUSB', 'ttyACM');  // Possibly several other names too
-    {$ENDIF}
-    {$IFDEF FREEBSD}
-    PortPrefix:array[0..2] of string = ('cuad', 'cuau', 'ttyu'); // http://www.freebsd.org/doc/handbook/serial.html
-    {$ENDIF}
-    {$IFDEF NETBSD}
-    PortPrefix:array[0..0] of string = ('cuad');  // Don't have better info at the moment
-    {$ENDIF}
-    {$IFDEF OPENBSD}
-    PortPrefix:array[0..0] of string = ('cuad');  // Don't have better info at the moment
-    {$ENDIF}
+{$IFDEF UNIX}
+var
+  {$IFDEF LINUX}
+  PortPrefix: array[0..1] of string = ({'ttyS',} 'ttyUSB', 'ttyACM');  // Possibly several other names too
   {$ENDIF}
+  {$IFDEF FREEBSD}
+  PortPrefix:array[0..2] of string = ('cuad', 'cuau', 'ttyu'); // http://www.freebsd.org/doc/handbook/serial.html
+  {$ENDIF}
+  {$IFDEF NETBSD}
+  PortPrefix:array[0..0] of string = ('cuad');  // Don't have better info at the moment
+  {$ENDIF}
+  {$IFDEF OPENBSD}
+  PortPrefix:array[0..0] of string = ('cuad');  // Don't have better info at the moment
+  {$ENDIF}
+{$ENDIF}
 
-  function GetSerialPortNames: string;
-  var
-    i: Integer;
-    sr : TSearchRec;
-    f: TSerialHandle;
-    val: integer;
-    ser: TSerialStruct;
-  begin
-    Result := '';
-    try
-      for i := 0 to high(PortPrefix) do
-        if FindFirst('/dev/' + PortPrefix[i] + '*', LongInt($FFFFFFFF), sr) = 0 then  // from patch
-        begin
-          repeat
-            if (sr.Attr and $FFFFFFFF) = Sr.Attr then
+function GetSerialPortNames: string;
+var
+  i: Integer;
+  sr : TSearchRec;
+  f: TSerialHandle;
+  val: integer;
+  t: Termios;
+begin
+  Result := '';
+  try
+    for i := 0 to high(PortPrefix) do
+      if FindFirst('/dev/' + PortPrefix[i] + '*', LongInt($FFFFFFFF), sr) = 0 then  // from patch
+      begin
+        repeat
+          if (sr.Attr and $FFFFFFFF) = Sr.Attr then
+          begin
             begin
+              f := FpOpen('/dev/'+sr.Name, O_RDWR or O_NOCTTY or O_NONBLOCK);
+              if f <> 0 then
               begin
-                f := FpOpen('/dev/'+sr.Name, O_RDWR or O_NOCTTY or O_NONBLOCK);
-                if f <> 0 then
-                begin
-                  val := FpIOCtl(f, TIOCGSERIAL, @ser);
-                  // Workaround for cp210x driver returning ENOTTY
-                  if (val <> -1) or (errno = ENOTTY) then
-                    Result := Result + '/dev/' + sr.Name + ',';
-                  SerClose(f);
-                end;
+                val := tcgetattr(f, t);
+                if (val <> -1) and (t.c_cflag > 0) then
+                  Result := Result + '/dev/' + sr.Name + ',';
+                SerClose(f);
               end;
             end;
-          until FindNext(sr) <> 0;
-        end;
-    finally
-      FindClose(sr);
-      // remove trailing comma
-      i := length(Result);
-      if (i > 0) and (Result[i] = ',') then
-        Result[i] := #0;
-    end;
+          end;
+        until FindNext(sr) <> 0;
+      end;
+  finally
+    FindClose(sr);
+    // remove trailing comma
+    i := length(Result);
+    if (i > 0) and (Result[i] = ',') then
+      Result[i] := #0;
   end;
-  {$ENDIF}
-////////////////////////////////////////////////////////////
+end;
+{$ENDIF}
+
 end.
 
